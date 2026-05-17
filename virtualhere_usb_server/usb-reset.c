02e1af0b5d6e7ed9a4768d8df8393281540ab584
timemaster5 <timemaster@trillinis.com>
2026-05-17 20:14:57 +0200

    Enhance USB reset with disconnect and reconnect

--- virtualhere_usb_server/usb-reset.c
+++ virtualhere_usb_server/usb-reset.c
@@ -39,9 +39,19 @@ static int build_device_path(const char *input, char *output, size_t output_size
     return 0;
 }
 
+static int warn_ioctl_failure(const char *name, const char *device_path) {
+    int saved_errno = errno;
+    fprintf(stderr, "%s %s failed: %s\n", name, device_path, strerror(saved_errno));
+    errno = saved_errno;
+    return -1;
+}
+
 int main(int argc, char **argv) {
     char device_path[128];
     int fd;
+    int disconnected = 0;
+    int reset_failed = 0;
+    int connect_failed = 0;
 
     if (argc != 2) {
         fprintf(stderr, "Usage: %s /dev/bus/usb/BBB/DDD|BBB/DDD\n", argv[0]);
@@ -53,19 +63,47 @@ int main(int argc, char **argv) {
         return 2;
     }
 
-    fd = open(device_path, O_WRONLY);
+    fd = open(device_path, O_RDWR);
     if (fd < 0) {
         fprintf(stderr, "open %s failed: %s\n", device_path, strerror(errno));
         return 1;
     }
 
+    if (ioctl(fd, USBDEVFS_DISCONNECT, 0) == 0) {
+        disconnected = 1;
+        printf("Disconnected %s from kernel drivers\n", device_path);
+        sleep(1);
+    } else {
+        warn_ioctl_failure("USBDEVFS_DISCONNECT", device_path);
+        fprintf(stderr, "Continuing with USBDEVFS_RESET fallback for %s\n", device_path);
+    }
+
     if (ioctl(fd, USBDEVFS_RESET, 0) < 0) {
-        fprintf(stderr, "USBDEVFS_RESET %s failed: %s\n", device_path, strerror(errno));
-        close(fd);
-        return 1;
+        warn_ioctl_failure("USBDEVFS_RESET", device_path);
+        reset_failed = 1;
+    } else {
+        printf("Reset %s OK\n", device_path);
+        sleep(1);
+    }
+
+    if (disconnected) {
+        if (ioctl(fd, USBDEVFS_CONNECT, 0) < 0) {
+            warn_ioctl_failure("USBDEVFS_CONNECT", device_path);
+            connect_failed = 1;
+        } else {
+            printf("Reconnected %s to kernel drivers\n", device_path);
+        }
     }
 
     close(fd);
-    printf("Reset %s OK\n", device_path);
+
+    if (reset_failed || connect_failed) {
+        return 1;
+    }
+
+    if (disconnected) {
+        printf("Disconnect/reset/connect completed for %s\n", device_path);
+    }
+
     return 0;
 }
